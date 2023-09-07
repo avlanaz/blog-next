4643aeb07b3b3bdd896dd37496efc862fafdfd6e
avlanaz <87753607+avlanaz@users.noreply.github.com>
2023-09-07 10:03:26 +1000

    Use URL env variable to fetch data from server side

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 async function getData() {
-	const res = await fetch(`/api/posts`, {
+	const res = await fetch(process.env.URL + "api/posts", {
 		cache: "no-store",
 	});
 
